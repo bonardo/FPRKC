7580b55d72d0fade6f3fcc307100f3bfdf9eba46
DESKTOP-4HCNUBF\A-BILAL <bilal_290995@yahoo.com>
2017-05-26 03:27:36 +0700

    Edit login, register, tambah icon logo

---
+++ Web/js/custom.js
@@ -0,0 +1,6 @@
+/**
+ * Created by A-BILAL on 5/26/2017.
+ */
+function register() {
+    alert("Registrasi Berhasil");
+}
---
+++ Web/login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A-BILAL
+ * Date: 5/26/2017
+ * Time: 3:02 AM
+ */
+header('location: index.html');
